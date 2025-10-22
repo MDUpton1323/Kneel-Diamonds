d61a954b526b4113ae696971e97c67842202a8cb
Marcus Upton <MarcusDUpton13@gmail.com>
2025-10-21 21:06:32 -0500

    Added metal, size, and style options

---
+++ scripts/SizeOptions.js
@@ -0,0 +1,16 @@
+export const SizeOptions = async () => {
+  const response = await fetch("https://localhost:8088/sizes");
+  const sizes = await response.json();
+
+  let optionsHTML = `<h2>Sizes</h2>`;
+
+  const divStringArray = sizes.map((size) => {
+    return `<div>
+        <input type="radio" name="size" value="${size.id}" /> ${size.carets}
+        </div>`;
+  });
+
+  optionsHTML += divStringArray.join("");
+
+  return optionsHTML;
+};
